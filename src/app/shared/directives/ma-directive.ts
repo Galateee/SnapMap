ef2666c448ab532f69f83296c7d741987848e7a5
VIDAL Robin <rvidal@et.intechinfo.fr>
2026-06-29 10:33:17 +0200

    initial commit

---
+++ src/app/shared/directives/ma-directive.ts
@@ -0,0 +1,8 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appMaDirective]',
+})
+export class MaDirective {
+  constructor() {}
+}
---
+++ src/app/shared/pipes/mon-pipe-pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'monPipe',
+})
+export class MonPipePipe implements PipeTransform {
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+}
